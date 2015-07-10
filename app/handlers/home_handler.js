652b54d59d16f618335ae97e0f42c3b458bba186
awesomegamer <avay_raul@hotmail.com>
2015-07-10 09:43:56 -0400

    example usage in home handler

--- app/handlers/home_handler.js
+++ app/handlers/home_handler.js
@@ -1,14 +1,29 @@
 module.exports = {
-	index: function(req, res) {
+	/*
+
+	{{handlername}}_handler.js
+	
+	get('/action', "handlername#action")
+
+	action: function(req, res) { -----> will automatically render views/{{handlername}}/action  
+		return {  ------> view data
+			data: "Some data",
+			render: "home/render" ------> override the default template
+		}
+	}
+
+	*/
+
+	index: function() {
 		return {
 			data: "hello"
 		}
 	},
 
-	show: function(req, res) {
+	show: function() {
 	},
 
-	forced: function(req, res) {
+	forced: function() {
 		return {
 			render: "home/render"
 		}
